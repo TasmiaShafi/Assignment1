d47df2cef9700bb6997ab783facdd130c12aa6b3
tshafi <tshafi627@gmail.com>
2021-04-04 15:25:23 +0500

    Adapter Set to Recycler View + Task Insertion Work is done

--- ToDoApp/app/src/main/java/com/example/todoapp/Adapter/ToDoAdapter.java
+++ ToDoApp/app/src/main/java/com/example/todoapp/Adapter/ToDoAdapter.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.todoapp.AddTask;
 import com.example.todoapp.MainActivity;
 import com.example.todoapp.Model.ToDoModel;
 import com.example.todoapp.R;
@@ -19,74 +19,85 @@ import com.example.todoapp.Utils.DatabaseHelper;
 
 import java.util.List;
 
-class ToDOAdapter extends RecyclerView.Adapter<ToDOAdapter.MyViewHolder>{
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.MyViewHolder> {
 
     private List<ToDoModel> mList;
     private MainActivity activity;
     private DatabaseHelper myDB;
-    public ToDOAdapter(DatabaseHelper DB,MainActivity activity){
-        this.activity=activity;
-        this.myDB=DB;
+
+    public ToDoAdapter(DatabaseHelper myDB , MainActivity activity){
+        this.activity = activity;
+        this.myDB = myDB;
     }
+
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.task_layout,parent,false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.task_layout , parent , false);
         return new MyViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        final ToDoModel item=mList.get(position);
+        final ToDoModel item = mList.get(position);
         holder.mCheckBox.setText(item.getTask());
         holder.mCheckBox.setChecked(toBoolean(item.getStatus()));
         holder.mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    myDB.updateStatus(item.getId(),1);
-                }
-                else {
-                    myDB.updateStatus(item.getId(),0);
-                }
+                if (isChecked){
+                    myDB.updateStatus(item.getId() , 1);
+                }else
+                    myDB.updateStatus(item.getId() , 0);
             }
         });
-
     }
+
     public boolean toBoolean(int num){
         return num!=0;
     }
-    //it'll be helpful when we creating the class to add new Task
+
     public Context getContext(){
         return activity;
+    }
+
+    public void setTasks(List<ToDoModel> mList){
+        this.mList = mList;
+        notifyDataSetChanged();
+    }
+
+    public void deletTask(int position){
+        ToDoModel item = mList.get(position);
+        myDB.deleteTask(item.getId());
+        mList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void editItem(int position){
+        ToDoModel item = mList.get(position);
+
+        Bundle bundle = new Bundle();
+        bundle.putInt("id" , item.getId());
+        bundle.putString("task" , item.getTask());
+
+        AddTask task = new AddTask();
+        task.setArguments(bundle);
+        task.show(activity.getSupportFragmentManager() , task.getTag());
+
+
     }
 
     @Override
     public int getItemCount() {
         return mList.size();
     }
-    public void setTasks(List<ToDoModel> mList){
-        this.mList=mList;
-        notifyDataSetChanged();
-    }
-    public void deleteTask(int position){
-        ToDoModel item=mList.get(position);
-        myDB.deleteTask(item.getId());
-        mList.remove(position);
-        notifyItemRemoved(position);
-    }
-    public void editItem(int position){
-        ToDoModel item=mList.get(position);
-        Bundle bundle=new Bundle();
-        bundle.putInt("id",item.getId());
-        bundle.putString("task",item.getTask());
-    }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
         CheckBox mCheckBox;
-        public MyViewHolder(@NonNull View itemView){
+        public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            mCheckBox=itemView.findViewById(R.id.mcheckbox);
+            mCheckBox = itemView.findViewById(R.id.mcheckbox);
         }
     }
 }
--- ToDoApp/app/src/main/java/com/example/todoapp/MainActivity.java
+++ ToDoApp/app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.todoapp.Adapter.ToDoAdapter;
+import com.example.todoapp.Model.ToDoModel;
 import com.example.todoapp.Utils.DatabaseHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnDialogCloseListner{
     private RecyclerView mRecyclerview;
     private FloatingActionButton fab;
     private DatabaseHelper myDB;
+    private List<ToDoModel> mList;
+    private ToDoAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +30,31 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerview=findViewById(R.id.recyclerView);
         fab=findViewById(R.id.fab);
         myDB=new DatabaseHelper(MainActivity.this);
+        mList=new ArrayList<>();
+        adapter=new ToDoAdapter(myDB,MainActivity.this);
+        mRecyclerview.setHasFixedSize(true);
+        mRecyclerview.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerview.setAdapter(adapter);
+        mList=myDB.getAllTasks();
+        Collections.reverse(mList); //to get latest one on the top
+        adapter.setTasks(mList);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AddTask.newInstance().show(getSupportFragmentManager(),AddTask.TAG);
 
             }
         });
+    }
+
+    @Override
+    public void onDialogClose(DialogInterface dialogInterface) {
+        mList=myDB.getAllTasks();
+        Collections.reverse(mList); //to get latest one on the top
+        adapter.setTasks(mList);
+        adapter.notifyDataSetChanged();
+
+
     }
 }
