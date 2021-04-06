7263d5d1c6bea48b0d35729353ce05cf6e10d80b
tshafi <tshafi627@gmail.com>
2021-04-06 20:11:34 +0500

    Text Animation added

--- ToDoApp/app/src/main/java/com/example/todoapp/SplasActivity.java
+++ ToDoApp/app/src/main/java/com/example/todoapp/SplasActivity.java
@@ -5,6 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class SplasActivity extends AppCompatActivity {
+    TextView text;
+    CharSequence charSequence;
+    int index;
+    long delay = 200;
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splas);
+        text=findViewById(R.id.todoText);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        textAnimation("To Do App");
+    }
+    Runnable runnable=new Runnable() {
+        @Override
+        public void run() {
+            text.setText(charSequence.subSequence(0,index++));
+            if(index<=charSequence.length()){
+                handler.postDelayed(runnable,delay);
+                startActivity(new Intent(SplasActivity.this, MainActivity.class));
+            }
+
+        }
+    };
+    public void textAnimation(CharSequence cs){
+        charSequence=cs;
+        index=0;
+        text.setText("");
+        handler.removeCallbacks(runnable);
+        handler.postDelayed(runnable,delay);
+    }
+}
+
+
+/*package com.example.todoapp;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.os.Handler;
 
 public class SplasActivity extends AppCompatActivity {
 
@@ -22,4 +68,4 @@ public class SplasActivity extends AppCompatActivity {
             }
         }, 4000);
     }
-}
+}*/
